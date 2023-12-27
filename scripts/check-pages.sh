7847e0e08cc9639c38438d9ada32d9a29cf61cdf
Sebastiaan Speck <12570668+sebastiaanspeck@users.noreply.github.com>
2023-12-27 18:10:57 +0100

    check-pages: add special case for '(' and ')'

--- scripts/check-pages.sh
+++ scripts/check-pages.sh
@@ -115,6 +115,7 @@ strip_commands() {
     grep "$COMMAND_REGEX" "$file" | 
     sed 's/{{[^}]*}}/{{}}/g' | 
     sed 's/<[^>]*>//g' | 
+    sed 's/([^)]*)//g' | 
     sed 's/"[^"]*"/""/g' | 
     sed "s/'[^']*'//g" | 
     sed 's/`//g'
