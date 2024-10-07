37aea2ea16425fd845dd459b758db2a7ddb6ed04
Sebastiaan Speck <12570668+sebastiaanspeck@users.noreply.github.com>
2024-10-07 13:57:00 +0200

    Update scripts/_common.py

--- scripts/_common.py
+++ scripts/_common.py
@@ -276,4 +276,4 @@ def generate_github_new_link(page):
     filename = urllib.parse.quote(Path(page).name)
 
     page = replace_characters_for_link(page)
-
+   return f"[{page}](https://github.com/tldr-pages/tldr/new/main/{directory}?filename={filename})"
